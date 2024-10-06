53c2403664b984fb6f6dbeb843af82cadb44322b
sandeep <grizzlybear986@gmail.com>
2024-10-06 23:50:11 +0530

    got calc methods working. little drunk!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total,current) => total+current,0);
 };
 
-const multiply = function() {
 
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a*b,1 )
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+    let answer = 1;
+    for(let i = 1; i<=a; i++){
+      console.log(i)
+      answer *= i;
+    }
+    return answer;
+  };
+
+console.log(factorial(0))
 
 // Do not edit below this line
 module.exports = {
